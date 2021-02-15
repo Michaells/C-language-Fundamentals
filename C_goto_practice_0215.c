d3be768e7e2d6bdb54b5482d79fd7c9e540ebfeb
Michaells <mikechenlnds@gmail.com>
2021-02-16 07:43:30 +0800

    Add files via upload

---
+++ C_goto_practice_0215.c
@@ -0,0 +1,30 @@
+//#include<stdio.h>
+//#include<string.h>
+//#include<Windows.h>
+//#include<stdlib.h>
+//int main()
+//{
+//	printf("haha\n");
+//	goto again;
+//	printf("hello");
+//again:
+//	printf("yeahyeah");
+//	return 0;
+//}
+//{
+//	char input[20] = { 0 };
+//	system("shutdown - s - t 120");
+//again:
+//	printf("attention your computer will shutdown in 120 seconds\nplease enter Pixel to stop the shutdown");
+//	scanf("%s", input);
+//	if (strcmp(input, "Pixel") == 0)
+//	{
+//		system("shutdown - a");
+//
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//	return 0;
+//}
