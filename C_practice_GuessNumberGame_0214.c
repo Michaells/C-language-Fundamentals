e2c33603d1d4a7b2210185e2e868c74ea252b299
Michaells <mikechenlnds@gmail.com>
2021-02-16 00:42:19 +0800

    Add files via upload

---
+++ C_practice_GuessNumberGame_0214.c
@@ -0,0 +1,66 @@
+//#include<stdio.h>
+//#include<string.h>
+//#include<Windows.h>
+//#include<stdlib.h>
+//#include<time.h>
+////²q¼Æ¦r¹CÀ¸
+//
+//void menu()
+//{
+//	printf("*************************************\n");
+//	printf("*****1. play  0. exit      **********\n");
+//	printf("*************************************\n");
+//
+//}
+//void game()
+//{
+//	printf("guess number game\n");
+//	// 1.randomly generate a number
+//	// 2.guess a number
+//	int ret;
+//	int guess = 0;
+//	ret = rand()%100+1;
+//	//printf("%d\n", ret);
+//	while (1)
+//	{
+//		printf("guess a number\n");
+//		scanf("%d", &guess);
+//		if (guess>ret)
+//		{
+//			printf("try smaller\n");
+//		}
+//		else if (guess<ret)
+//		{
+//			printf("try bigger\n");
+//		}
+//		else
+//		{
+//			printf("Congrats, Got it!!");
+//			break;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int input = 0;
+//	srand((unsigned int)time(NULL)); //timestamp
+//	do
+//	{
+//		menu();
+//		printf("\nplease choose=>:");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("exit game\n");
+//			break;
+//		default:
+//			printf("system wrong\n");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
